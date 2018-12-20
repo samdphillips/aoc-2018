fce2f8210e88b6860170495390abcbd18b108df2
Sam Phillips <samdphillips@gmail.com>
2018-12-19 21:28:06 -0800

    Day 14
    Oh yeah, lists are super slow.  Especially when you're repeatedly 
    appending and checking the length.  Changed to a pre-allocated vector 
    and now it is much faster.

--- 14.rkt
+++ 14.rkt
@@ -9,19 +9,26 @@
         (digits q (cons r acc)))))
 
 (define (run num-recipes [seed '(3 7)])
-  (for/fold ([rs seed]
-             [i 0]
-             [j 1]
-             #:result (take (drop rs num-recipes) 10))
+  (define rs (make-vector (+ num-recipes 12)))
+  (define lrs 0)
+  (define (add-rs! vs)
+    (for ([i (in-naturals lrs)]
+          [v (in-list vs)])
+      (vector-set! rs i v))
+    (set! lrs (+ lrs (length vs))))
+  (add-rs! seed)
+
+  (for/fold ([i 0] [j 1])
             ([x (in-naturals)]
-             #:break (> (length rs) (+ 10 num-recipes)))
-    (define a (list-ref rs i))
-    (define b (list-ref rs j))
-    (let* ([rs  (append rs (digits (+ a b)))]
-           [lrs (length rs)])
-      (values rs
-              (modulo (+ i a 1) lrs)
-              (modulo (+ j b 1) lrs)))))
+             #:break (> lrs (+ 10 num-recipes)))
+    (define a (vector-ref rs i))
+    (define b (vector-ref rs j))
+    (add-rs! (digits (+ a b)))
+    (values (modulo (+ i a 1) lrs)
+            (modulo (+ j b 1) lrs)))
+
+  (for/list ([i (in-range num-recipes (+ 10 num-recipes))])
+    (vector-ref rs i)))
 
 (module+ test
   (require rackunit)
@@ -31,9 +38,7 @@
   (check-equal? (run 2018) '(5 9 4 1 4 2 9 8 8 2))
   )
 
-#;
 (module+ main
-  (let ([rs (run 12 '(0 0))])
-    (for ([i (in-range 1 (- (length rs) 10))])
-      (displayln (~a "[" (~a #:width 4 #:align 'right i) "] "
-                     (take (drop rs i) 10))))))
+  (displayln "PART ONE")
+  (displayln
+    (apply ~a (time (run 681901)))))

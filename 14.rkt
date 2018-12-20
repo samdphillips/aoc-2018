59daa4bf09f5bae6267d8f1fd050a589775ce98d
Sam Phillips <samdphillips@gmail.com>
2018-12-19 22:13:54 -0800

    Day 14
    Part two works on the tests and is now horribly slow on the real puzzle.

--- 14.rkt
+++ 14.rkt
@@ -41,3 +41,48 @@
   (displayln "PART ONE")
   (displayln
     (apply ~a (time (run 681901)))))
+
+(define (run2 pattern [seed '(3 7)])
+  (define plen (length pattern))
+  (define rs (make-vector 10))
+  (define lrs 0)
+  (define (resize!)
+    (define new-size (* 2 lrs))
+    (displayln (~a "new size: " new-size))
+    (define old rs)
+    (set! rs (make-vector new-size))
+    (vector-copy! rs 0 old))
+  (define (add-rs! vs)
+    (unless (< (+ lrs (length vs)) (vector-length rs))
+      (resize!))
+    (for ([i (in-naturals lrs)]
+          [v (in-list vs)])
+      (vector-set! rs i v))
+    (set! lrs (+ lrs (length vs))))
+  (add-rs! seed)
+  (define (match?)
+    (and (>= lrs plen)
+         (for/and ([v (in-list pattern)]
+                   [i (in-naturals (- lrs plen))])
+           (= v (vector-ref rs i)))))
+
+  (let repeat ([i 0] [j 1])
+    (unless (match?)
+      (define a (vector-ref rs i))
+      (define b (vector-ref rs j))
+      (add-rs! (digits (+ a b)))
+      (repeat (modulo (+ i a 1) lrs)
+              (modulo (+ j b 1) lrs))))
+
+  (- lrs plen))
+
+(module+ test
+  (check-equal? (run2 '(5 1 5 8 9)) 9)
+  (check-equal? (run2 '(0 1 2 4 5)) 5)
+  (check-equal? (run2 '(9 2 5 1 0)) 18)
+  (check-equal? (run2 '(5 9 4 1 4)) 2018))
+
+(module+ main
+  (displayln "PART TWO")
+  (displayln
+    (time (run2 '(6 8 1 9 0 1)))))
